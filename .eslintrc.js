7089938b3b5d9a183eab6147529f0c82046a1be9
Abdullah Al Kafi <kafikafi1922@gmail.com>
2024-07-29 17:45:26 +0600

    first helllo world

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+ 
+    'no-trailing-spaces': 'off', 
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      env: {
+        development: {
+          rules: {
+            'no-trailing-spaces': 'off',
+          },
+        },
+      },
+    },
+  ],
 };
