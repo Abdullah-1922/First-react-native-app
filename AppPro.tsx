ebd3bc9db51f088c8f2e2adf0dc4851f4972aef0
Abdullah Al Kafi <kafikafi1922@gmail.com>
2024-07-29 18:25:57 +0600

    Text color base on theme - dark -light

---
+++ AppPro.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  useColorScheme,
+  SafeAreaView,
+} from 'react-native';
+
+const AppPro = (): JSX.Element => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text  style={isDarkMode ? styles.whiteText : styles.darkText}>
+          Hello world
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  whiteText: {
+    color: '#FFFFFF',
+  },
+  darkText: {
+    color: '#000000',
+  },
+});
+
+export default AppPro;
